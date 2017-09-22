c67fbeae58cee8ca547740f49bdd146c84f0dc31
Interview <interview@interview-jnolan-2.local>
2017-09-22 13:52:51 -0700

    fix route start add nodemon

--- src/routes/restaurant.js
+++ src/routes/restaurant.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 const restaurants = require('../data/restaurants.json').restaurants;
 
-router.get('/', function (req, res, next) {
-  const restaurant = restaurants.find(item => item.id === 1906);
+router.get('/:id', function (req, res, next) {
+	let id=parseInt(req.params.id);
+  const restaurant = restaurants.find(item => item.id === id);
 
   res.render('restaurant', restaurant);
 });
