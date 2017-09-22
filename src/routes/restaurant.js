f0df0908827fc0f8bf78d11c90ec0ac781c5c3ab
mattia richetto <mattia.richetto@gmail.com>
2017-09-22 09:21:33 -0700

    fix head title

--- src/routes/restaurant.js
+++ src/routes/restaurant.js
@@ -6,8 +6,10 @@ const restaurants = require('../data/restaurants.json').restaurants;
 
 router.get('/:id', (req, res) => {
   const restaurant = restaurants.find(item => item.id === parseInt(req.params.id, 10));
+  const model = Object.assign({}, restaurant);
+  model.title = `${restaurant.name} - ${restaurant.city} | AnotherTable`;
 
-  res.render('restaurant', restaurant);
+  res.render('restaurant', model);
 });
 
 module.exports = router;
