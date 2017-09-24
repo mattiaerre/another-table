ee3b6af159d08b3b08e34634b11b1998266c741f
mattia richetto <mattia.richetto@gmail.com>
2017-09-23 19:47:13 -0700

    [WIP] search (#6)
    * search and autocomplete (wip)
    
    * search and autocomplete (wip)
    
    * moar autocomplete (wip)

---
+++ src/public/javascripts/autocomplete.js
@@ -0,0 +1,12 @@
+(({ cities, delay }) => {
+  let timer;
+  const input = document.getElementById('autocomplete-input');
+  input.onkeyup = ((ev) => {
+    clearTimeout(timer);
+    const value = ev.target.value;
+    timer = setTimeout(() => {
+      const results = cities.filter(city => (city.toLowerCase().includes(value.toLowerCase())));
+      console.log(results);
+    }, delay);
+  });
+})(__AUTOCOMPLETE__);
