9742c0e0d533f6cad423d38fa10898d1e68420b1
Mukul Raina <mukulraina7@gmail.com>
2017-09-27 11:09:16 -0700

    Tasks 1 and 3

--- src/routes/reactaurant.js
+++ src/routes/reactaurant.js
@@ -4,7 +4,7 @@ const restaurants = require('../data/restaurants.json').restaurants;
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const restaurant = restaurants.find(item => item.id === 1906);
+  const restaurant = restaurants.find(item => item.id === req.params.restaurant_id);
   res.render('reactaurant', restaurant);
 });
 
