1af8bf64346aa411d98b4b12a3b82ed72197aa5e
mattia richetto <mattia.richetto@gmail.com>
2017-09-27 23:45:52 -0700

    stats widget

--- src/client/app/App.js
+++ src/client/app/App.js
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => (
-  <h2>Stats Widget</h2>
-);
+const nullStats = {
+  $: 0,
+  $$: 0,
+  $$$: 0,
+  $$$$: 0,
+};
+
+const makeStats = (data) => { // eslint-disable-line
+  return data.reduce((stats, restaurant) => {
+    const copy = Object.assign({}, stats);
+    switch (restaurant.price) {
+      case 1:
+        copy.$ += 0;
+        break;
+      case 2:
+        copy.$$ += 1;
+        break;
+      case 3:
+        copy.$$$ += 1;
+        break;
+      case 4:
+        copy.$$$$ += 1;
+        break;
+      default:
+        break;
+    }
+    return copy;
+  }, nullStats);
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      stats: nullStats
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/v1/restaurants')
+      .then(response => response.json())
+      .then(({ data }) => {
+        const stats = makeStats(data);
+        this.setState({ stats });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <div className="price-range">
+          <ul>
+            {
+              Object.keys(this.state.stats).map(key => (
+                <li key={key}>{key} ({this.state.stats[key]})</li>
+              ))
+            }
+          </ul>
+          <div>Number of restaurants per price range.</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
