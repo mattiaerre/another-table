ef60398fff2822b2b36cf6a0bd0e5bd3e1ccf589
mattia richetto <mattia.richetto@gmail.com>
2018-05-10 22:11:57 -0700

    add .prettierrc

--- src/app.js
+++ src/app.js
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
   next(err);
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
