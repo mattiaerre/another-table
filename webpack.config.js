79699aa25a8aaa93462c05bb98d1572dd19c263e
mattia richetto <mattia.richetto@gmail.com>
2018-11-25 21:39:16 -0800

    update deps and s/client/app/app (#16)

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack'); // eslint-disable-line no-unused-vars
 const path = require('path');
 
 const BUILD_DIR = path.resolve(__dirname, 'src/public/javascripts');
-const APP_DIR = path.resolve(__dirname, 'src/client/app');
+const APP_DIR = path.resolve(__dirname, 'src/app');
 
 const config = {
   entry: `${APP_DIR}/index.js`,
