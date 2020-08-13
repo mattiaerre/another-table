4a5f8501317c228874d026a68a4a64fb343fdd51
Mattia Richetto <mattia.richetto@gmail.com>
2020-08-12 20:54:22 -0700

    use resizer url (#29)

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const { restaurants } = require('../data/restaurants');
+const { restart } = require('nodemon');
 
 const router = express.Router();
 
+function blocklist(restaurant) {
+  return ![27691, 74941, 4057, 76462, 83107].includes(restaurant.id);
+}
+
 router.get('/', (req, res) => {
-  const model = { title: 'Search', restaurants };
+  const model = {
+    title: 'Search',
+    restaurants: restaurants.filter(blocklist).map(restaurant => ({
+      ...restaurant,
+      resizer_url: `https://resizer.otstatic.com/v2/profiles/legacy/${restaurant.id}.jpg`
+    }))
+  };
   res.render('search', model);
 });
 
