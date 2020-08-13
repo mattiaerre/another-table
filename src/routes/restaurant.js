b0afbcb4294e686c619bc3b059c20ad834687fcb
mattia richetto <mattia.richetto@gmail.com>
2020-08-12 21:39:46 -0700

    cleanup & fix

--- src/routes/restaurant.js
+++ src/routes/restaurant.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { restaurants } = require('../data/restaurants.json');
+const factory = require('./factory');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const restaurant = restaurants.find((item) => item.id === 1906);
+  const restaurant = factory(restaurants.find((item) => item.id === 1906));
   res.render('restaurant', restaurant);
 });
 
